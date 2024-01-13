1affde7623e92a3eefefc939b7c6ee23b5d31c4e
Phil Whisenhunt <phil.whisenhunt@slope.io>
2024-01-13 17:04:26 -0500

    Now staying, but wiping title accidentally

--- app/javascript/controllers/viewings_controller.js
+++ app/javascript/controllers/viewings_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
 
     createAndView() {
-        console.log("Clicky")
+
         const url = this.element.dataset.url;
         const csrfToken = document.head.querySelector('meta[name="csrf-token"]').content;
 
@@ -15,7 +15,7 @@ export default class extends Controller {
         })
             .then(response => response.text())
             .then(html => {
-                const frame = document.getElementById('films'); // Adjust the ID if needed
+                const frame = document.getElementById(this.element.id); // Adjust the ID if needed
                 frame.innerHTML = html;
             });
     }
