4901a519ec17bb96263790bdd8096fad4edc6fc1
Phil Whisenhunt <phil.whisenhunt@slope.io>
2024-01-14 15:30:37 -0500

    Try this to solve missing file

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,7 +5,7 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus = application
-import ViewingsController from "../controllers/viewings_controller";
+import ViewingsController from "controllers/viewings_controller";
 application.register("viewings", ViewingsController);
 
 export { application }
